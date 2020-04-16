5684a7389667a025da9281a6b0a9ff6e55efe239
Shakhzod Kholikov <imshakhzod@gmail.com>
2020-04-16 02:45:08 +0200

    Updates

---
+++ precache-manifest.4601521a76e67ce5f9677a5906168ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16320a0b8e61ba19378ea07af462b9a1",
+    "url": "/https//covid-19-status.github.io/index.html"
+  },
+  {
+    "revision": "6624210a7cd5136a6e4b",
+    "url": "/https//covid-19-status.github.io/static/css/main.e5fc6e9e.chunk.css"
+  },
+  {
+    "revision": "2c5d6e423350044a8a86",
+    "url": "/https//covid-19-status.github.io/static/js/2.51925157.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/https//covid-19-status.github.io/static/js/2.51925157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6624210a7cd5136a6e4b",
+    "url": "/https//covid-19-status.github.io/static/js/main.97c926ec.chunk.js"
+  },
+  {
+    "revision": "dcc80f9e19155d34ee60",
+    "url": "/https//covid-19-status.github.io/static/js/runtime-main.01bd33e0.js"
+  }
+]);
